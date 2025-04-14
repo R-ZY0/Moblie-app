39f5ec3779bb7e1ce4b769373d395a54c57d60b7
Mohamed ramzy <148167890+R-ZY0@users.noreply.github.com>
2025-04-14 15:45:04 +0200

    first commit

---
+++ lib/my_contact.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+// Ensure the package is added to your pubspec.yaml file before importing it.
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+ import 'package:url_launcher/url_launcher.dart';
+
+
+class MyContacts extends StatelessWidget {
+  const MyContacts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(27, 38, 59, 1),
+      body: SizedBox(
+        width: double.infinity,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 100),
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/f1.jpg'),
+                radius: 100,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text('Mohamed Ramzy',
+                  style: TextStyle(
+                    color: const Color.fromRGBO(224, 225, 221, 1),
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('+01015286683',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.grey,
+                      )),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(65, 90, 119, 1),
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.phone,
+                        size: 30,
+                        color: const Color.fromRGBO(224, 225, 221, 1),
+                      ),
+                      onPressed: () {
+                        launchUrl(Uri.parse('tel:+01015286683'));
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              GridView.builder(
+                itemCount: 9,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3),
+                itemBuilder: (context, index) {
+
+                  List<CircleAvatar> aFvatars = [
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Color.fromRGBO(27, 38, 59, 1),
+                      child: GestureDetector(
+                        onTap: () {
+                          launchUrl(Uri.parse('https://www.facebook.com/ramzy.mohamed.1238'));
+                        },
+                        child: Icon(
+                          FontAwesomeIcons.facebook,
+                          color: Colors.blue,
+                          size: 50,
+                        ),
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Color.fromRGBO(27, 38, 59, 1),
+                      child: Icon(
+                        FontAwesomeIcons.whatsapp,
+                        color: Colors.green,
+                        size: 50,
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Color.fromRGBO(27, 38, 59, 1),
+                      child: Icon(
+                        FontAwesomeIcons.linkedin,
+                        color: Colors.blue,
+                        size: 50,
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Color.fromRGBO(27, 38, 59, 1),
+                      child: Icon(
+                        FontAwesomeIcons.instagram,
+                        color: Colors.pink,
+                        size: 50,
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Color.fromRGBO(27, 38, 59, 1),
+                      child: Icon(
+                        FontAwesomeIcons.github,
+                        color: Colors.black,
+                        size: 50,
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Color.fromRGBO(27, 38, 59, 1),
+                      child: Icon(
+                        FontAwesomeIcons.snapchatGhost,
+                        color: Colors.yellow,
+                        size: 50,
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Color.fromRGBO(27, 38, 59, 1),
+                      child: Icon(
+                        FontAwesomeIcons.tiktok,
+                        color: Colors.redAccent,
+                        size: 50,
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Color.fromRGBO(27, 38, 59, 1),
+                      child: Icon(
+                        FontAwesomeIcons.twitter,
+                        color: Colors.blue,
+                        size: 50,
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Color.fromRGBO(27, 38, 59, 1),
+                      child: Icon(
+                        FontAwesomeIcons.telegram,
+                        color: Colors.blue,
+                        size: 50,
+                      ),
+                    ),
+                  ];
+                  return aFvatars[index % aFvatars.length];
+                },
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                padding: EdgeInsets.all(8.0),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
